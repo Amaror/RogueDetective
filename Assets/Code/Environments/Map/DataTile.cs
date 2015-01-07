a18874738de2f8edd1d73e7ccf1ca97abb232a0c
Amaror <jj_schmid@gmx.com>
2015-01-07 13:56:52 +0100

    Xml Vorbereitung und kleine Änderungen

--- Assets/Code/Environments/Map/DataTile.cs
+++ Assets/Code/Environments/Map/DataTile.cs
@@ -1,10 +1,19 @@
-﻿public class DataTile {
+﻿using UnityEngine;
 
+public class DataTile {
+
+	//Boundaries
+	public Vector2 _boundarySouth;
+	public Vector2 _boundaryNorth;
+	//Information
 	public string _type;
 	public string _streetName;
 	public int _number;
 
-	public DataTile(string type, string streetName, int number) {
+
+	public DataTile(string type, string streetName, int number, Vector2 north, Vector2 south) {
+		_boundaryNorth = north;
+		_boundarySouth = south;
 		_type = type;
 		_streetName = streetName;
 		_number = number;
--- Assets/Code/Environments/Map/District.cs
+++ Assets/Code/Environments/Map/District.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class District{
 
@@ -10,8 +11,14 @@ public class District{
 	public int _y;
 	//Blocks
 	public Block[] _blocks;
+	//Tags to allow or disallow various blocks
+	public List<string> _tags;
 
-	public District(int south, int north) {
+	public District(Vector2 south, Vector2 north) {
+		//Setup Variables
+		_tags = new List<string> ();
+		_tags.Add("Base");
+
 		_boundarySouth = south;
 		_boundaryNorth = north;
 		_x = (int)_boundaryNorth.x - (int)_boundarySouth.x;
